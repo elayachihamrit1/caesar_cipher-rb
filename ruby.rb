d4d5b620bc5cdee56be6787ee9209ba157cc5665
elayachiamritoff <elayachiamritoff@gmail.com>
2023-12-05 14:10:20 +0100

    Check if the char is letter

--- ruby.rb
+++ ruby.rb
@@ -2,10 +2,14 @@
 
     def encrypte(string, shift_factor)
 
+        decrypted_text = ""
+
         #iterate through each char in the string
         string.each_char do |char|
+            
+            #Check if the character is a letter.
+            if char.match(/[A-Za-z]/)
 
-        puts char
 
         end
 
