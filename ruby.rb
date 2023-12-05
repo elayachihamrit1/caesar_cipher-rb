06d1649b553b6deaaa104e6d8b9ea7f8cac8e936
elayachiamritoff <elayachiamritoff@gmail.com>
2023-12-05 14:01:26 +0100

    Iterate through each char

--- ruby.rb
+++ ruby.rb
@@ -2,8 +2,15 @@
 
     def encrypte(string, shift_factor)
 
+        #iterate through each char in the string
+        string.each_char do |char|
 
+        puts char
 
+        end
 
 
     end
+
+
+    encrypte("Hello", 7)
